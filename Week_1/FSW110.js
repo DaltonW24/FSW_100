8bbc16bbcb32632393806b6b9cb1726714d90655
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-03-26 15:40:55 -0400

    Added CHanges due to Mistake

--- Week_1/FSW110.js
+++ Week_1/FSW110.js
@@ -1,9 +1,2 @@
-function randomNumber(){
-var min = Number(1)
-var max = Number(6)
-
-var firstNumber = Math.floor(Math.random()* max - min + 1)
-document.getElementById("number").textContent = firstNumber
-}
-    randomNumber();
-    
+console.log(Document)
+ 
