e95be1c0e1777cdeecb592860b93e149773bf9cd
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-02 10:47:06 -0400

    Added my week 2 work for FSW110

---
+++ Week_2/FSW110_Assignment2.js
@@ -0,0 +1,21 @@
+var newH1 = document.createElement('h1');
+newH1.textContent = "Welcome to my JS site";
+document.body.appendChild(newH1);
+
+
+var newP1 = document.createElement("p");
+newP1.textContent = "All of this was created with Javascript";
+document.body.appendChild(newP1);
+
+
+var newList = document.createElement('ol');
+document.body.append()
+var newListItem1 = document.createElement('li');
+newListItem1.textContent = "cake";
+document.body.appendChild('ol')
+var newListItem2 = document.createElement("li");
+newListItem2.textContent = "Spaghetti";
+document.body.appendChild("ol")
+var newListItem3 = document.createElement("li");
+newListItem3.textContent = "Meatballs";
+document.body.appendChild("ol")
