515589d6a0942ef8ac827d5b69c97a102122df2d
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-02 11:04:11 -0400

    Added my secind assignemnt for FSW110 in Week 2

---
+++ Week_2/FSW110Assignment2Week2.js
@@ -0,0 +1,15 @@
+var newHeader = document.createElement("h1");
+newHeader.textContent = "My Webpage";
+document.body.append(newHeader);
+
+var newP = document.createElement("p");
+newP.textContent = "content"
+document.body.appendChild(newP);
+
+var newList = document.createElement("ol")
+newList.textContent = "List Content"
+document.body.appendChild(newList);
+
+var newFooter = document.createElement("footer");
+newFooter.textContent = "Footer"
+document.body.appendChild(newFooter);
