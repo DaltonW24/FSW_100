0f3d8a955e89916418bb08b0587fc36cb74afb45
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-09 13:31:53 -0400

    added some more files

--- Week_3/FSW110Week3Assignment1.js
+++ Week_3/FSW110Week3Assignment1.js
@@ -5,53 +5,13 @@ document.body.appendChild(newHeader);
 newHeader.style.color = "red";
 newHeader.style.font = 'georgia';
 
-var newList = document.createElement('ul')
-newList.setAttribute('namesList')
-document.body.appendChild(newList);
+var namesList = ['steve', 'larry', 'joe','shirley', 'steph', 'nate', 'rick', 'emily'];
+var newList = document.getElementById('namesList')
 
-var newListItem1 = document.createElement('li');
-newListItem1.textContent = "steve"
-document.newList.appendChild(newListItem1)
-newListItem1.setAttribute("listSteve")
-
-var newListItem2 = document.createElement('li');
-newListItem2.textContent = "larry";
-document.newList.appendChild(newListItem2)
-newListItem2.setAttribute("listLarry");
-
-var newListItem3 = document.createElement('li');
-newListItem4.textContent = "joe";
-document.newList.appendChild(newListItem3)
-newListItem3.setAttribute("listJoe");
-
-var newListItem4 = document.createElement('li');
-newListItem4.textContent = "shirley";
-document.newList.appendChild(newListItem4)
-newListItem4.setAttribute("listShirley");
-
-var newListItem5 = document.createElement('li');
-newListItem4.textContent = "steph";
-document.newList.appendChild(newListItem5)
-newListItem5.setAttribute("listSteph");
-
-var newListItem6 = document.createElement('li');
-newListItem4.textContent = "nate";
-document.newList.appendChild(newListItem6)
-newListItem6.setAttribute("listNate");
-
-var newListItem7 = document.createElement('li');
-newListItem4.textContent = "rick";
-document.newList.appendChild(newListItem7)
-newListItem7.setAttribute("listRick");
-
-var newListItem8 = document.createElement('li');
-newListItem4.textContent = "emily";
-document.newList.appendChild(newListItem8)
-newListItem8.setAttribute("listEmily");
-
-function headerDisplay(newHeader)
-for (let i = 0; i < 10, i++;) {
-    console.log(newHeader);
-  }
+for (var i = 0; i < namesList.length; i++) {
+	
+	var personName = document.createElement('li')
+	personName.textContent = namesList[i]
+	newList.append(personName);
 
 //const names = [“steve”, “larry”, “joe”, “shirley”, “steph”, “nate”, “rick”, “emily”];
---
+++ Week_3/FSWWeek3Assignment2.js
@@ -0,0 +1,11 @@
+var myHeader =document.createElement('h2');
+myHeader.setAttribute(id , 'header')
+myHeader.textContent = "The Headliner";
+document.body.appendChild('header');
+
+myHeader.style.cssText = "font-size: 20px";
+myHeader.style.cssText = 'font-weight: lighter';
+myHeader.style.cssText = 'font-family: sans-serif';
+myHeader.style.cssText = "color: conrflowerblue";
+
+myHeader.classList.add('.border')
