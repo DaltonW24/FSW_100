64d24ee748a3ad7abd49a8478e28db85603bc13e
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-14 15:47:48 -0400

    Added Fixes

--- Week_3/FSW110Week3Assignment1.js
+++ Week_3/FSW110Week3Assignment1.js
@@ -5,13 +5,26 @@ document.body.appendChild(newHeader);
 newHeader.style.color = "red";
 newHeader.style.font = 'georgia';
 
+for (var i =0; i < 10 ; i++) {
+	newHeader = document.createElement('h1');
+newHeader.textContent = "Hello World";
+document.body.appendChild(newHeader);
+
+}
+const nodeList = document.querySelectorAll('h1')
+
 var namesList = ['steve', 'larry', 'joe','shirley', 'steph', 'nate', 'rick', 'emily'];
-var newList = document.getElementById('namesList')
+var newList = document.createElement('ul');
+
+for (var i = 0; i < nodeList.length; i++) {
+	nodeList[i].style.color= 'red'	
+}
 
 for (var i = 0; i < namesList.length; i++) {
 	
 	var personName = document.createElement('li')
 	personName.textContent = namesList[i]
 	newList.append(personName);
-
-//const names = [“steve”, “larry”, “joe”, “shirley”, “steph”, “nate”, “rick”, “emily”];
+}
+document.body.appendChild(newList);
+// const names = [“steve”, “larry”, “joe”, “shirley”, “steph”, “nate”, “rick”, “emily”];
--- Week_3/FSWWeek3Assignment2.js
+++ Week_3/FSWWeek3Assignment2.js
@@ -1,11 +1,14 @@
-var myHeader =document.createElement('h2');
 
-myHeader.textContent = "The Headliner";
-document.body.appendChild(myHeader);
+for (var i = 0; i < 5; i++) {
+	var myHeader = document.createElement('h2');
+    myHeader.setAttribute("class", ".border");
 
-myHeader.style.cssText = "font-size: 20px";
-myHeader.style.cssText = 'font-weight: lighter';
-myHeader.style.cssText = 'font-family: sans-serif';
-myHeader.style.cssText = "color: conrflowerblue";
 
-myHeader.classList.add('.border')
+    myHeader.textContent = "The Headliner";
+    document.body.appendChild(myHeader);
+
+    
+    myHeader.style.cssText = "font-size: 20px", 
+    myHeader.style.cssText = "font-weight: lighter"
+    myHeader.style.cssText = "color: cornflowerblue"
+}
