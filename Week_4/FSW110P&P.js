90d0b8b87ded35dec305a212c92a1deb86db6b55
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-20 15:10:17 -0400

    Completed FSW110P&P

---
+++ Week_4/FSW110P&P.js
@@ -0,0 +1,33 @@
+var square = document.createElement('div');
+square.className = "squared";
+document.body.appendChild(square);
+square.style.width = "350px"
+square.style.height =  "350px"
+square.style.backgroundColor = "white";
+
+    window.addEventListener('load', function(){
+        square.style.backgroundColor = "black"
+    })
+    
+
+    square.addEventListener('mouseover', function(){
+        square.style.backgroundColor = "green"
+    })
+    
+
+    square.addEventListener("mousedown", function(){
+        square.style.backgroundColor = "Yellow"
+    })
+
+    square.addEventListener("mouseup", function(){
+        square.style.backgroundColor ="blue"
+    })
+    
+
+    square.addEventListener('dblclick', function(){
+        square.style.backgroundColor ="red"
+    })
+
+    square.addEventListener('scroll', function(){
+        square.style.backgroundColor = "purple"
+    })
---
+++ Week_4/FSW110Project.js
@@ -0,0 +1,43 @@
+var inputBox1 = document.createElement('input');
+
+inputBox1.style.border = "solid black 2px";
+
+inputBox1.textContent ="content1";
+document.body.appendChild(inputBox1);
+console.log(inputBox1);
+// var buttonAdd = document.body.createElement("button");
+    
+// var buttonSub = document.body.createElement("button");
+
+// var buttonMult = document.body.createElement("button");
+
+// var buttonDiv = document.body.createElement("button");
+
+
+
+
+// if (add){
+// function add(valueA, valueB){
+//    return valueA + valueB;
+//     }
+// }
+
+// else if(subtract){
+// function subtract(){
+//     return a - b;
+//     }
+// }
+
+// else if(multiply){
+// function multiply(){
+//     return a * b;
+//     }
+// }
+// else if(divide){
+// function divide(){
+//     return a / b;
+//     }
+// }
+// else {
+//     console.log("N/A Please try Again")
+// }
