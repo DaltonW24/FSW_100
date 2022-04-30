e4c5ea5e4e36ae582367b6066904e59a7d312cd3
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-04-30 12:52:14 -0400

    Started Responsive web page

--- Week_4/FSW110Project.js
+++ Week_4/FSW110Project.js
@@ -19,3 +19,24 @@ function divideButtonFun(){
     })
     
 }
+
+Head1.style.cssText = 'color:red'
+
+divAdd.style.cssText = 'margin: 10px'
+divAdd1.style.cssText = 'background-color: yellow'
+divAdd2.style.cssText = 'background-color: yellow'
+
+
+divSub.style.cssText = 'margin: 10px'
+SubBox1.style.cssText = 'background-color: aqua'
+SubBox2.style.cssText = "background-color: aqua"
+
+
+divMult.style.cssText = 'margin: 10px'
+multBox1.style.cssText = 'background-color: lime'
+multBox2.style.cssText = 'background-color: lime'
+
+
+divDivide.style.cssText = "margin: 10px"
+divideBox1.style.cssText = 'background-color: cornflowerblue'
+divideBox2.style.cssText = 'background-color: cornflowerblue'
