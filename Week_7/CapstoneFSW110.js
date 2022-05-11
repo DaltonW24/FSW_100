eb139b4f6e2da4d15cce545f01d192fb9afe38f0
DaltonW24 <98478993+DaltonW24@users.noreply.github.com>
2022-05-11 11:51:01 -0400

    Delete CapstoneFSW110.js

--- Week_7/CapstoneFSW110.js
+++
@@ -1,7 +0,0 @@
-function proceedForm(){
-    document.getElementById('Proceed').addEventListener('click', function(){
-        document.getElementById('ListItem1').value = document.getElementById('Name');
-        document.getElementById('ListItem2').value = document.getElementById('Date'); 
-    })
-}
-proceedForm(); 
