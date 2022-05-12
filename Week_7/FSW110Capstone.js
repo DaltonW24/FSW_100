dc5f493cb462c72120896a85fbd35d6d8c207635
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-05-12 19:41:57 -0400

    Finished Capstone FSW110

--- Week_7/FSW110Capstone.js
+++ Week_7/FSW110Capstone.js
@@ -1,12 +1,25 @@
-document.getElementById('Submit').addEventListener('click', function(){
-    const unorderedList = document.createElement('ul')
-    unorderedList.setAttribute(id, 'unorderedList')
+document.getElementById('Submit').addEventListener('click', function(e){
+e.preventDefault()
+    let unorderedList = document.createElement('ul')
+    unorderedList.setAttribute('id', 'unorderedList')
+    document.body.appendChild(unorderedList)
 
-    const listItem1 = document.createElement('li')
-    listItem1.setAttribute(id, 'listItem1')
-    listItem1.textContent = document.getElementsByClassName('BestPlayerResponse')
+    let listItem1 = document.createElement('li')
+    listItem1.setAttribute('id', 'listItem1')
+    listItem1.textContent = document.getElementById('BestPlayerResponse').value
+  console.log(document.getElementById('BestPlayerResponse'))
 
-    const listItem2 = document.createElement('li')
-    listItem2.setAttribute(id, 'listItem2')
-    listItem2.textContent = document.getElementsByClassName('Question2')
+    let listItem2 = document.createElement('li')
+    listItem2.setAttribute('id', 'listItem2')
+
+        var radios = document.getElementsByName('radio');
+        
+        for (var radio of radios){
+            if (radio.checked) {
+                listItem2.textContent = radio.value
+            }
+        }
+    unorderedList.append(listItem1, listItem2);
+    
 })
+
